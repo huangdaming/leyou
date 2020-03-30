204b276ffca45114ee7544658f91e170648487f7
huangdaming <861634426@qq.com>
2020-03-30 09:55:18 +0800

    Initial commit

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+/**
+ * @author huangdaming
+ * @MapperScan mapper接口的包扫描
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.leyou.item.mapper")
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class, args);
+    }
+}
